30b2d83a2957e97a0d0f7fef7f4a8dce769ce2b8
derintekin <tekinderin5@gmail.com>
2024-04-16 22:06:18 -0400

    added code

---
+++ itis3135/project/js.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("homeBtn").addEventListener("click", function() {
+      window.location.href = "index.html";
+    });
+  
+    document.getElementById("aboutBtn").addEventListener("click", function() {
+      window.location.href = "about.html";
+    });
+  
+    document.getElementById("productsBtn").addEventListener("click", function() {
+      window.location.href = "products.html";
+    });
+  
+    document.getElementById("contactBtn").addEventListener("click", function() {
+      window.location.href = "contact.html";
+    });
+  });
+  
