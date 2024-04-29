596b935f25e97fbd16a74dfb1a0d19cd6d9644cb
derintekin <tekinderin5@gmail.com>
2024-04-28 23:38:26 -0400

    changes

--- itis3135/project/js.js
+++ itis3135/project/js.js
@@ -3,12 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
   
     buttons.forEach(function(button) {
       button.addEventListener("click", function() {
-        // Remove 'active' class from all buttons
         buttons.forEach(function(btn) {
           btn.classList.remove("active");
         });
   
-        // Add 'active' class to the clicked button
         this.classList.add("active");
       });
     });
